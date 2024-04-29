67776cb3fc835efffe7d17250e85824c3b5def91
tomasdarracq <tomasdarracq12@gmail.com>
2024-04-29 20:01:10 -0300

    .

---
+++ backend/obligatorioPenca/obligatorioPenca/src/main/java/com/obligatorioPenca/obligatorioPenca/ObligatorioPencaApplication.java
@@ -0,0 +1,13 @@
+package com.obligatorioPenca.obligatorioPenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatorioPencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatorioPencaApplication.class, args);
+	}
+
+}
---
+++ backend/obligatorioPenca/obligatorioPenca/src/test/java/com/obligatorioPenca/obligatorioPenca/ObligatorioPencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obligatorioPenca.obligatorioPenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioPencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
